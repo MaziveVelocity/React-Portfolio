291bf90795df0a08ca4ca912cf05fc6607ce57f9
Cayman Gill <cayman.g@hotmail.com>
2021-11-07 11:04:43 -0500

    modified build

---
+++ precache-manifest.46f607c311a62c42cad8db291da589e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8882bac934584f50fcefb51aa7baf05",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d6daa71a1de0f0daa11f",
+    "url": "/React-Portfolio/static/css/main.f1c2c564.chunk.css"
+  },
+  {
+    "revision": "d704982ccbc1269200ee",
+    "url": "/React-Portfolio/static/js/2.64a39278.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-Portfolio/static/js/2.64a39278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5429d20cff54c8a98d3e",
+    "url": "/React-Portfolio/static/js/3.cd368a3a.chunk.js"
+  },
+  {
+    "revision": "d6daa71a1de0f0daa11f",
+    "url": "/React-Portfolio/static/js/main.9d65f616.chunk.js"
+  },
+  {
+    "revision": "4023b61403b0a493aea1",
+    "url": "/React-Portfolio/static/js/runtime-main.dc1fde15.js"
+  }
+]);
