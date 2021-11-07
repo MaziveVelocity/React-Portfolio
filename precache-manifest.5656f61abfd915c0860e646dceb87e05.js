38b4d307f7d3f0733579ded791554e2e4a62d6f5
Cayman Gill <cayman.g@hotmail.com>
2021-11-07 11:18:34 -0500

    preping for build

---
+++ precache-manifest.5656f61abfd915c0860e646dceb87e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe15b208971f736b6f490cb260e4b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80c1b8ceac2286b380f5",
+    "url": "/static/css/main.e4d38063.chunk.css"
+  },
+  {
+    "revision": "d704982ccbc1269200ee",
+    "url": "/static/js/2.64a39278.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.64a39278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5429d20cff54c8a98d3e",
+    "url": "/static/js/3.cd368a3a.chunk.js"
+  },
+  {
+    "revision": "80c1b8ceac2286b380f5",
+    "url": "/static/js/main.79c0b0e6.chunk.js"
+  },
+  {
+    "revision": "e5d4cef5cfb795bc6a17",
+    "url": "/static/js/runtime-main.7a7f15ca.js"
+  }
+]);
