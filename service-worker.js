55d8106d26bda62efc81e2f1bf09751c59ef3a17
Cayman Gill <cayman.g@hotmail.com>
2021-11-07 12:06:52 -0500

    updated build

--- service-worker.js
+++ service-worker.js
@@ -11,30 +11,29 @@
  * See https://goo.gl/2aRDsh
  */
 
- importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
- importScripts(
-   "/React-Portfolio/precache-manifest.6a8f3bf4ddef6a833ac00c6568ac65e8.js"
- );
- 
- self.addEventListener('message', (event) => {
-   if (event.data && event.data.type === 'SKIP_WAITING') {
-     self.skipWaiting();
-   }
- });
- 
- workbox.core.clientsClaim();
- 
- /**
-  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-  * requests for URLs in the manifest.
-  * See https://goo.gl/S9QRab
-  */
- self.__precacheManifest = [].concat(self.__precacheManifest || []);
- workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
- 
- workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
-   
-   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
- });
- 
+importScripts(
+  "/React-Portfolio/precache-manifest.6a8f3bf4ddef6a833ac00c6568ac65e8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
